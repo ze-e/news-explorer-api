b829af89281f6a9713f6e68970169627ad7267bb
zrr <zrexrodriguez@gmail.com>
2020-12-17 22:12:17 -0800

    fix: fixed delete article error

--- models/Article.js
+++ models/Article.js
@@ -52,7 +52,7 @@ const articleSchema = new mongoose.Schema({
 
 articleSchema.statics.doesUserOwn = function (articleId, ownerId) {
   return this.findById(articleId).select('+owner')
-    //eslint is wrong. you cannot use === here
+    // eslint-disable-next-line eqeqeq
     .then((article) => (article.owner._id == ownerId ? article : Promise.reject(new Error('User does not own article'))))
     .catch(() => Promise.reject(new Error('User does not own article')));
 };
